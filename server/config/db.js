5d2aaa2a06de81302ce14a476eea7d4c4fd862ac
Syntaxhafeez <syntaxhafeez@gmail.com>
2025-02-19 18:18:44 +0530

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+// Function to connect with MongoDB database
+const connectDB = async () => {
+
+    mongoose.connection.on("connected", ()  => console.log('Database connected'));
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
+
+}
+
+export default connectDB;
